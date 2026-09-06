9980e338828342f1c080791e71757c4ce48c47bc
Tanisha Jain <tanijain2018@gmail.com>
2026-09-06 16:28:01 +0530

    fix: configure SMTP email backend for Django 6

--- sahayak/settings.py
+++ sahayak/settings.py
@@ -125,18 +125,15 @@ STATIC_URL = 'static/'
 #     },
 # }
 # Email configuration
-MAILERS = {
-    "default": {
-        "BACKEND": "django.core.mail.backends.smtp.EmailBackend",
-        "OPTIONS": {
-            "host": os.getenv("EMAIL_HOST"),
-            "port": int(os.getenv("EMAIL_PORT", 587)),
-            "username": os.getenv("EMAIL_HOST_USER"),
-            "password": os.getenv("EMAIL_HOST_PASSWORD"),
-            "use_tls": os.getenv("EMAIL_USE_TLS", "True") == "True",
-        },
-    },
-}
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+# DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
